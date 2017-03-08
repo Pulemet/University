65ad3637840b6d2d7d2ad12ba349195898efba72
Pulemet <Pulemet@DESKTOP-1I7R5K9>
2017-03-09 00:07:30 +0300

    Add project files.

---
+++ University/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(University.Startup))]
+namespace University
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
