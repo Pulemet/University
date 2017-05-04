5822946a39643af1874d9c4f91bc30e1440c3434
PliatnevI <PliatnevI@PC48>
2017-05-04 17:16:15 +0300

    Added classes for information about teachers in subjects.

---
+++ University/Models/Dto/TeacherDto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using University.Models.Helper;
+using University.Models.Tables;
+
+namespace University.Models.Dto
+{
+    public class TeacherDto
+    {
+        public TeacherDto()
+        {
+            Subjects = new List<Subject>();
+            Comments = new List<CommentToTeacherDto>();
+        }
+
+        public TeacherDto(ApplicationUser user)
+        {
+            Id = user.Id;
+            FirstName = user.FirstName;
+            SurName = user.SurName;
+            PatronymicName = user.PatronymicName;
+            Photo = user.Photo == "" ? ConstDictionary.NO_IMAGE : user.Photo;
+            Subjects = new List<Subject>();
+            Comments = new List<CommentToTeacherDto>();
+        }
+
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string SurName { get; set; }
+
+        public string PatronymicName { get; set; }
+
+        public string Photo { get; set; }
+
+        public string Department { get; set; }
+
+        public List<Subject> Subjects { get; set; }
+
+        public List<CommentToTeacherDto> Comments { get; set; } 
+    }
+}
