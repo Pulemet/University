8c77469d7f02a66c9f43db3d028b0e0ab215318c
Ekaterina Runets <ekaterina_runec@mail.ru>
2017-05-10 00:43:27 +0200

    Changing messages view.

--- University/Scripts/communion.js
+++ University/Scripts/communion.js
@@ -10,7 +10,6 @@
 }
 
 function SendMessage() {
-    event.preventDefault();
     var data = $('#sendMessage').serialize();
     var url = $('#sendMessage').attr('action');
     $.post(url, data, function (responce) {
@@ -93,7 +92,7 @@ function GetDialogHtml(dialog) {
 }
 
 function GetMessageHtml(message) {
-    return '<div class="msg-wrap" style="border-bottom: 1px solid #ddd; padding: 5px; background-color: aliceblue; border-radius: 3px"><div class="media msg">' +
+    return '<div class="msg-wrap" style="border-bottom: 1px solid #4c4444; padding: 5px; background-color: aliceblue; border-radius: 3px"><div class="media msg">' +
             '<small class="pull-right time"><i class="fa fa-clock-o" style="color: #6E6E6E;"> ' + message.DateSend +
             '</i></small><h5 class="media-heading" style="color: #003bb3;"><b>' + message.SurName + ' ' + message.FirstName +
             '</b></h5><small>' + message.Text + '</small></div></div>';
